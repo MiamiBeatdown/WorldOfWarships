466280b60ddbefc884609a6ec2b304c0604c1a85
MiamiBeatdown <alesha.ivanov.2000.steam@mail.ru>
2023-04-26 00:26:46 +0300

    onTouchEvent

--- com/example/worldofwarships/view/SeaView.java
+++ com/example/worldofwarships/view/SeaView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -12,6 +13,8 @@ import androidx.annotation.Nullable;
 public class SeaView extends View {
 
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private float redX = 0;
+    private float redY = 0;
 
     public SeaView(Context context) {
         super(context);
@@ -43,7 +46,33 @@ public class SeaView extends View {
         paint.setStyle(Paint.Style.STROKE);
         for(int i = 1; i < 11; i++)
             for(int j = 1; j < 11; j++)
-                canvas.drawRect(step*i, step*j, step*(i+1),step*(j+1), paint);
+                canvas.drawRect(step * i, step * j, step * (i + 1), step * (j + 1), paint);
+        if(redX != 0 && redY !=0) {
+            paint.setColor(Color.RED);
+            paint.setStyle(Paint.Style.FILL);
+            canvas.drawRect(step * redX, step * redY, step * (redX + 1), step * (redY + 1), paint);
+        }
         canvas.restore();
+    }
+
+    public boolean onTouchEvent(MotionEvent event){
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            float x = event.getX();
+            float y = event.getY();
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.RED);
+            float step = getWidth()/12;
+            if(getWidth() > getHeight())
+                step = getHeight()/12;
+            for(int i = 1; i < 10; i++)
+                for(int j = 1; j < 10; j++)
+                    if(step*i < x && x < step*(i+1) && step*j < y && y < step*(j+1)){
+                        redX = i;
+                        redY = j;
+                    }
+            invalidate();
+            return true;
+        }
+        return super.onTouchEvent(event);
     }
 }
