1a5c696f73845a3c89ce2e2e314ae213c75b7e41
MiamiBeatdown <alesha.ivanov.2000.steam@mail.ru>
2023-04-25 23:38:38 +0300

    View

--- com/example/worldofwarships/view/SeaView.java
+++ com/example/worldofwarships/view/SeaView.java
@@ -1,7 +1,8 @@
-package view;
+package com.example.worldofwarships.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
@@ -10,7 +11,7 @@ import androidx.annotation.Nullable;
 
 public class SeaView extends View {
 
-    private Paint paint;
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     public SeaView(Context context) {
         super(context);
@@ -31,16 +32,18 @@ public class SeaView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int step = getWidth()/12;
-       // canvas.save();
-        paint.setColor(0x80A6FF);
+        float step = getWidth()/12;
+        if(getWidth() > getHeight())
+            step = getHeight()/12;
+        canvas.save();
+        paint.setColor(Color.CYAN);
         paint.setStyle(Paint.Style.FILL);
-        canvas.drawRect(step, step, getWidth() - step, getWidth() - step, paint);
-        paint.setColor(0xFFFFFF);
+        canvas.drawRect(step, step, step*10, step*10, paint);
+        paint.setColor(Color.BLACK);
         paint.setStyle(Paint.Style.STROKE);
         for(int i = 1; i < 11; i++)
             for(int j = 1; j < 11; j++)
                 canvas.drawRect(step*i, step*j, step*(i+1),step*(j+1), paint);
-      //  canvas.restore();
+        canvas.restore();
     }
 }
